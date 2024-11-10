0520f2a3a509e56c1fc6d8a28cfbed07ed9f0dbb
sergio.mendonca <stmfmendonca@rd.com.br>
2024-11-10 15:36:38 -0300

    Adiciona algumas urls fakes de exemplo

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -1,7 +1,19 @@
 package util;
 
 public class Constants {
+    // Sites Maliciosos e Ofensivos
+    public static final String MALICIOUS_SITE_1 = "http://malicious-site1.com";
+    public static final String MALICIOUS_SITE_2 = "http://dangerous-website.com";
+    public static final String MALICIOUS_SITE_3 = "http://phishing-site.net";
+    public static final String MALICIOUS_SITE_4 = "http://fakebanking.com";
 
+    // Sites de Apostas (Bets)
+    public static final String BETTING_SITE_1 = "http://sportsbettingexample.com";
+    public static final String BETTING_SITE_2 = "http://gamblingplatform.com";
+    public static final String BETTING_SITE_3 = "http://bettingwebsite.com";
+    public static final String BETTING_SITE_4 = "http://online-casino.com";
+
+    // Outros exemplos de URLs
     public static final String URL_ZERO = "0.0.0.0";
     public static final String SPACE = "        ";
     public static final String GLOBO = "https://www.globo.com/";
