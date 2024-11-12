6617e3f145cc9160b8200f2e2205eabde7bd4062
sergio.mendonca <stmfmendonca@rd.com.br>
2024-11-12 18:28:26 -0300

    Muda url para oficial do arquivo hosts do windows.

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -1,17 +1,6 @@
 package util;
 
 public class Constants {
-    // Sites Maliciosos e Ofensivos
-    public static final String MALICIOUS_SITE_1 = "http://malicious-site1.com";
-    public static final String MALICIOUS_SITE_2 = "http://dangerous-website.com";
-    public static final String MALICIOUS_SITE_3 = "http://phishing-site.net";
-    public static final String MALICIOUS_SITE_4 = "http://fakebanking.com";
-
-    // Sites de Apostas (Bets)
-    public static final String BETTING_SITE_1 = "http://sportsbettingexample.com";
-    public static final String BETTING_SITE_2 = "http://gamblingplatform.com";
-    public static final String BETTING_SITE_3 = "http://bettingwebsite.com";
-    public static final String BETTING_SITE_4 = "http://online-casino.com";
 
     // Outros exemplos de URLs
     public static final String URL_ZERO = "0.0.0.0";
@@ -19,7 +8,6 @@ public class Constants {
 
     public static final String GLOBO1 = "www.globo.com";
     public static final String GLOBO2 = "globo.com";
-
-    public static final String FILE_PATH = "myFile.txt";
+    public static final String FILE_PATH = "C:\\Windows\\System32\\drivers\\etc\\hosts";
 
 }
